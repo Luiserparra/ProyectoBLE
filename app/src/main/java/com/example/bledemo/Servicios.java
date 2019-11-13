cee67ef5f858baa2ed7d7b2cd38db01f44493a2e
Luiserparra <parral700@gmail.com>
2019-11-12 22:16:33 -0500

    Notificaciones agregadas

--- app/src/main/java/com/example/bledemo/Servicios.java
+++ app/src/main/java/com/example/bledemo/Servicios.java
@@ -137,6 +137,8 @@ public class Servicios extends Fragment {
                     caracteristicas[j] = carac;
                     j++;
                 }
+
+
                 datosAEnviar.putStringArray("caracteristicas", caracteristicas);
 
                 //Inicializar el fragment de caracteristicas y mandar los datos
